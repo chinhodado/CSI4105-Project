7d50744155b9641ae9b17277fac11567b74f3811
Trung Do <chin.bimbo@gmail.com>
2016-02-27 15:46:35 -0500

    Add get number of cycles to example

--- graph_demo.py
+++ graph_demo.py
@@ -7,11 +7,15 @@ G.add_nodes_from([1,2,3])
 G.add_edges_from([(1,2),(1,3),(2,3)])
 
 # or generate graphs from built in methods
-K_5 = nx.complete_graph(5)
+k5 = nx.complete_graph(5)
 er = nx.erdos_renyi_graph(20, 0.10)
 red = nx.random_lobster(20, 0.5, 0.5)
 star = nx.star_graph(10)
 
+# get the number of basis cycles in G
+num_cycle = len(nx.cycle_basis(k5))
+print("Number of cycles: " + str(num_cycle))
+
 # draw and show
-nx.draw_circular(star, node_size=20)
+nx.draw_circular(k5, node_size=20)
 plt.show()
