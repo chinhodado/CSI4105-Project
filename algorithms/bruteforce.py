7400c761a2ba224feb0ce7b2feb36b05330b2932
Trung Do <chin.bimbo@gmail.com>
2016-03-05 12:10:18 -0500

    Use dynamic programming for the first bruteforce algorithm - speed it up at least a few hundred times!

--- algorithms/bruteforce.py
+++ algorithms/bruteforce.py
@@ -10,15 +10,26 @@ class Bruteforce(FeedbackVertexSetAlgorithm):
     A bruteforce algorithm to find the min size feedback vertex set
     This algorithm will find all nodes that belong to at least one cycle. For each of those nodes,
     it recursively run the algorithm on the induced subgraph with the node removed to find the min fbvs
-    
+
     WARNING: THERE IS NO GUARANTEE THAT THIS ALGORITHM IS CORRECT!!! E.G. THERE'S NO GUARANTEE THAT THE
     RETURNED FBVS IS THE MINIMUM ONE!
     """
 
+    def __init__(self):
+        # a dict to hold previously-computed min fvbs
+        # the key is a frozenset of nodes, and the value is the min fvbs of the subgraph induced on that set of nodes
+        self.cacheDict = {}
+
     def get_fbvs(self, graph):
+        # reset the cache dict
+        self.cacheDict = {}
+        return self._get_fbvs(graph)
+
+    def _get_fbvs(self, graph):
         # get the list of cycles
         cycles = nx.cycle_basis(graph)
 
+        # if the graph is already acyclic, there's nothing to remove, so return an empty set
         if len(cycles) == 0:
             return set()
 
@@ -32,9 +43,19 @@ class Bruteforce(FeedbackVertexSetAlgorithm):
             # make an induced subgraph with the current node removed
             nodes = graph.nodes()
             nodes.remove(node)
-            new_graph = graph.subgraph(nodes)
+            nodes_set = frozenset(nodes)
 
-            nodes_to_remove = self.get_fbvs(new_graph)
+            if nodes_set in self.cacheDict:
+                # if we have previously calculated the min fbvs for this induced subgraph, get that
+                # fbvs from the cache dict
+                nodes_to_remove = self.cacheDict[nodes_set]
+            else:
+                # otherwise we have to calculate it
+                new_graph = graph.subgraph(nodes)
+                nodes_to_remove = self._get_fbvs(new_graph)
+
+                # add the newly calculated fbvs to the cache dict
+                self.cacheDict[nodes_set] = nodes_to_remove
 
             if len(nodes_to_remove) < min_num_to_remove:
                 min_num_to_remove = len(nodes_to_remove)
