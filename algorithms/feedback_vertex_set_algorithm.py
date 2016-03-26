2b068fa2835ae8c01b18f3ba83d8c5acfb58eb3d
Trung Do <chin.bimbo@gmail.com>
2016-03-25 21:45:41 -0400

    Fix docstrings

--- algorithms/feedback_vertex_set_algorithm.py
+++ algorithms/feedback_vertex_set_algorithm.py
@@ -12,6 +12,7 @@ class FeedbackVertexSetAlgorithm(object):
         """
         Get a feedback vertex set of the given graph. May or may not be optimal.
         This will return a set of vertex whose removal will make the graph acyclic.
+
         :param graph: The graph to solve
         :return: A feedback vertex set for the graph
         """
@@ -21,6 +22,7 @@ class FeedbackVertexSetAlgorithm(object):
     def get_fbvs_max_size(self, graph, k) -> set:
         """
         Get a feedback vertex set of size at most k in the given graph. Return None if no such fbvs exists.
+
         :param graph: The graph to solve
         :param k: The maximum size for the fbvs
         :return: A fbvs of size at most k, or None if no such fbvs exists
