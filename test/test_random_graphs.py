caf050b50b0cd0688186b0e5523ce3d06364fdcd
Trung Do <chin.bimbo@gmail.com>
2016-03-28 00:28:58 -0400

    Update test

--- test/test_random_graphs.py
+++ test/test_random_graphs.py
@@ -5,6 +5,8 @@ import networkx as nx
 from algorithms.bruteforce import Bruteforce
 from algorithms.bruteforce_cycle import BruteforceCycle
 from algorithms.iterative_compression import IterativeCompression
+from algorithms.maximum_induced_forest import MaximumInducedForest
+from algorithms.randomized import Randomized
 
 """
 Use some randomly-generated graphs to make sure the algorithms return the same value
@@ -16,8 +18,10 @@ def test_algorithms(algorithms, graph: nx.Graph):
     print("Testing graph with {0} nodes and {1} edges".format(graph.number_of_nodes(), graph.number_of_edges()))
     results = []
     for algorithm, name in algorithms:
+        # make a copy of the graph in case the algorithm mutates it
+        graph_copy = graph.copy()
         start_time = time.time()
-        result = len(algorithm.get_fbvs(graph))
+        result = len(algorithm.get_fbvs(graph_copy))
         print("{0}: {1}, time: {2}".format(name, result, time.time() - start_time))
         results.append(result)
     assert results.count(results[0]) == len(results), "The algorithms's results are not the same!"
@@ -26,8 +30,14 @@ def test_algorithms(algorithms, graph: nx.Graph):
 bruteforce_cycle = BruteforceCycle()
 bruteforce = Bruteforce()
 ic = IterativeCompression()
+rn = Randomized()
+mif = MaximumInducedForest()
 
-algorithms = [[bruteforce_cycle, "bruteforce_cycle"], [bruteforce, "bruteforce"], [ic, "itertive_compression"]]
+algorithms = [[bruteforce_cycle, "bruteforce_cycle"],
+              [bruteforce, "bruteforce"],
+              [ic, "itertive_compression"],
+              # [rn, "randomized_opt"], # disabled because too slow
+              [mif, "maximum_induced_forest"]]
 
 graph = nx.erdos_renyi_graph(9, 0.5)
 test_algorithms(algorithms, graph)
