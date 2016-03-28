bb3db48bc928c93ba8840555c86382d0eb7c5ba7
Trung Do <chin.bimbo@gmail.com>
2016-03-28 00:26:39 -0400

    Update test

--- test/test_deterministic_graph.py
+++ test/test_deterministic_graph.py
@@ -4,6 +4,8 @@ import time
 from algorithms.bruteforce import Bruteforce
 from algorithms.bruteforce_cycle import BruteforceCycle
 from algorithms.iterative_compression import IterativeCompression
+from algorithms.maximum_induced_forest import MaximumInducedForest
+from algorithms.randomized import Randomized
 
 """
 Test the algorithms using deterministic/pre-defined graphs with already known solution
@@ -12,8 +14,10 @@ Test the algorithms using deterministic/pre-defined graphs with already known so
 
 def test_algorithms(algorithms, graph, expected: int):
     for algorithm, name in algorithms:
+        # make a copy of the graph in case the algorithm mutates it
+        graph_copy = graph.copy()
         start_time = time.time()
-        result = len(algorithm.get_fbvs(graph))
+        result = len(algorithm.get_fbvs(graph_copy))
         print("{0}: {1}, time: {2}".format(name, result, time.time() - start_time))
         assert expected == result, "Wrong result!"
     print()
@@ -21,8 +25,14 @@ def test_algorithms(algorithms, graph, expected: int):
 bruteforce_cycle = BruteforceCycle()
 bruteforce = Bruteforce()
 ic = IterativeCompression()
+rn = Randomized()
+mif = MaximumInducedForest()
 
-algorithms = [[bruteforce_cycle, "bruteforce_cycle"], [bruteforce, "bruteforce"], [ic, "itertive_compression"]]
+algorithms = [[bruteforce_cycle, "bruteforce_cycle"],
+              [bruteforce, "bruteforce"],
+              [ic, "itertive_compression"],
+              # [rn, "randomized_opt"], # disabled because too slow
+              [mif, "maximum_induced_forest"]]
 
 print("Testing graph with 2 cycles that can be broken by removing 1 node (node 0)", end="\n")
 graph = nx.Graph()
@@ -36,18 +46,12 @@ graph.add_nodes_from(range(1, 16))
 graph.add_cycle([10,11,12])
 test_algorithms(algorithms, graph, 1)
 
-print("Testing a K5 complete graph", end="\n")
-graph = nx.complete_graph(5)
-test_algorithms(algorithms, graph, 3)
+for i in range(3, 15):
+    print("Testing a K{} complete graph".format(i), end="\n")
+    graph = nx.complete_graph(i)
+    test_algorithms(algorithms, graph, i - 2)
 
-print("Testing a K7 complete graph", end="\n")
-graph = nx.complete_graph(7)
-test_algorithms(algorithms, graph, 5)
-
-print("Testing an acyclic graph (star5)", end="\n")
-graph = nx.star_graph(5)
-test_algorithms(algorithms, graph, 0)
-
-print("Testing an acyclic graph (star7)", end="\n")
-graph = nx.star_graph(7)
-test_algorithms(algorithms, graph, 0)
+for i in range(5, 8):
+    print("Testing an acyclic graph (star{})".format(i), end="\n")
+    graph = nx.star_graph(i)
+    test_algorithms(algorithms, graph, 0)
